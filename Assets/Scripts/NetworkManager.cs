6077a6647c4bbcca726f6d5fe410a0c8ddfb0d1b
TCalvert93 <tcalvert93@gmail.com>
2014-01-24 20:08:26 -0500

    Added NetworkManager

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+    private string gameName = "Lastgate World 1";
+    private string typeName = "Lastgate";
+    private int maxConnections = 25;
+    private int port = 25000;
+
+	// Use this for initialization
+    void Start() { startServer();  }
+	
+	// Update is called once per frame
+	void Update () {}
+
+    public void startServer()
+    {
+        Network.InitializeServer(maxConnections, port, !Network.HavePublicAddress());
+        MasterServer.RegisterHost(typeName, gameName);
+    }
+
+    void OnServerStart()
+    {
+        Debug.Log("server up");
+    }
+}
