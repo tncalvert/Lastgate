6b49908aea6be28d4a3d977ba7a04e480addebf2
TCalvert93 <tcalvert93@gmail.com>
2014-01-25 16:09:07 -0500

    Added animations for Dwarf man

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -3,7 +3,14 @@ using System.Collections;
 
 public class Control : MonoBehaviour {
 
+    private Animator animator;
+
 	// Use this for initialization
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
 	void Start () {
 	
 	}
@@ -12,17 +19,49 @@ public class Control : MonoBehaviour {
 	void Update () {
 
 		if (networkView.isMine || (!Network.isServer && !Network.isClient /* Not networked at all */)) {
-			if (Input.GetKey ("left")) {
+
+            
+
+			if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+                animator.SetInteger("Direction", 2);
 				transform.position += new Vector3 (-1, 0);
-			} else if (Input.GetKey ("right")) {
+			} else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+                animator.SetInteger("Direction", 0);
 				transform.position += new Vector3 (1, 0);
 			}
 
-			if (Input.GetKey ("up")) {
+			if (Input.GetKeyDown ("up")) {
+                animator.SetInteger("Direction", 1);
 				transform.position += new Vector3 (0, 1);
-			} else if (Input.GetKey ("down")) {
+			} else if (Input.GetKeyDown ("down")) {
+                animator.SetInteger("Direction", 3);
 				transform.position += new Vector3 (0, -1);
 			}
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow) ||
+                Input.GetKeyDown(KeyCode.RightArrow) ||
+                Input.GetKeyDown(KeyCode.DownArrow) ||
+                Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                animator.SetBool("Moving", true);
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftArrow) ||
+                Input.GetKeyUp(KeyCode.RightArrow) ||
+                Input.GetKeyUp(KeyCode.DownArrow) ||
+                Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                animator.SetBool("Moving", false);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                animator.SetBool("Attack", true);
+            }
+            else if (Input.GetKeyUp(KeyCode.Space))
+            {
+                animator.SetBool("Attack", false);
+            }
 		}
 
 		// Send update to network
