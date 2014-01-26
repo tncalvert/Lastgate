3a88bb185ecc7fcba28f88a2ff6e987de0725908
process <justin.chines@gmail.com>
2014-01-26 02:39:31 -0500

    Added splash screen

---
+++ Assets/Scripts/Menus/SplashLogic.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashLogic : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		Debug.Log ("FUCK");
+		yield return new WaitForSeconds(4f);
+		Application.LoadLevel (1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
